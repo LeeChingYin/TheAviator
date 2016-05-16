4196765ca95bf20c7eb39a50f4a2e43926d647b7
LeeChingYin <leechingyin09@gmail.com>
2016-05-17 00:11:32 +0800

    no message

--- build/bundle.js
+++ build/bundle.js
@@ -46,7 +46,7 @@
 
 	__webpack_require__(1);
 
-	__webpack_require__(5);
+	var THREE = __webpack_require__(5);
 
 	var Colors = {
 		red:0xf25346,
@@ -64,12 +64,115 @@
 
 		createLights();
 
-		createPlane();
+		// createPlane();
 		createSea();
-		createSky();
+		// createSky();
 
-		loop();
+		// loop();
 
+	}
+
+	var scene, camera, fieldOfView, aspectRatio, nearPlane, 
+	farPlane, HEIGHT, WIDTH, renderer, container;
+
+	function createScene() {
+		HEIGHT = window.innerHeight;
+		WIDTH = window.innerWidth;
+
+		scene = new THREE.Scene();
+
+		scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
+
+		aspectRatio = WIDTH / HEIGHT;
+		fieldOfView = 60;
+		nearPlane = 1;
+		farPlane = 10000;
+		camera = new THREE.PerspectiveCamera(
+			fieldOfView,
+			aspectRatio,
+			nearPlane,
+			farPlane
+			);
+
+		camera.position.x = 0;
+		camera.position.z = 200;
+		camera.position.y = 100;
+
+		renderer = new THREE.WebGLRenderer({
+			alpha: true,
+
+			antialias: true
+		});
+
+		renderer.setSize(WIDTH, HEIGHT);
+
+		renderer.shadowMap.enabled = true;
+
+		container = document.getElementById("world");
+		container.appendChild(renderer.domElement);
+
+		window.addEventListener('resize', handleWindowResize, false);
+	}
+
+	function handleWindowResize() {
+		HEIGHT = window.innerHeight;
+		WIDTH = window.innerWidth;
+		renderer.setSize(WIDTH, HEIGHT);
+		camera.aspect = WIDTH / HEIGHT;
+		camera.updateProjectionMatrix();
+	}
+
+	var hemisphereLight, shadowLight;
+
+	function createLights(){
+		hemisphereLight = new THREE.HemisphereLight(0xaaaaa, 0x00000, .9);
+		shadowLight = new THREE.DirectionalLight(0xffff, .9);
+
+		shadowLight.position.set(150, 350, 350);
+
+		shadowLight.castShadow = true;
+
+		shadowLight.shadow.camera.left = -400;
+		shadowLight.shadow.camera.right = 400;
+		shadowLight.shadow.camera.top = 400;
+		shadowLight.shadow.camera.bottom = -400;
+		shadowLight.shadow.camera.near = 1;
+		shadowLight.shadow.camera.far = 1000;
+
+
+		shadowLight.shadow.mapSize.width = 2048;
+		shadowLight.shadow.mapSize.height = 2048;
+
+		scene.add(hemisphereLight);
+		scene.add(shadowLight);
+
+	}
+
+	Sea = function() {
+
+		var geom = new THREE.CylinderGeometry(600, 600, 800, 40, 10);
+
+		geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
+
+		var mat = new THREE.MeshPhongMaterial({
+			color: Colors.blue,
+			transparent: true,
+			opacity: .6,
+			shading: THREE.FlatShading,
+		});
+
+		this.mesh = new THREE.Mesh(geom, mat);
+
+		this.mesh.receiveShadow = true;
+	}
+
+	var sea;
+	function createSea() {
+		sea = new Sea();
+
+		sea.mesh.position.y = -600;
+
+		scene.add(sea.mesh);
 	}
 
 /***/ },
