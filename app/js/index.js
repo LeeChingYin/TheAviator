c64944409741cdfbe262fce9a2af338db2df5b11
LeeChingYin <leechingyin09@gmail.com>
2016-05-18 00:36:18 +0800

    no message

--- app/js/index.js
+++ app/js/index.js
@@ -20,7 +20,7 @@ function init(){
 
 	// createPlane();
 	createSea();
-	// createSky();
+	createSky();
 
 	// loop();
 
@@ -127,4 +127,96 @@ function createSea() {
 	sea.mesh.position.y = -600;
 
 	scene.add(sea.mesh);
+}
+
+Cloud = function(){
+	this.mesh = new THREE.Object3D();
+
+	var geom = new THREE.BoxGeometry(20, 20, 20);
+
+	var mat = new THREE.MeshPhongMaterial({
+		color: Colors.white
+	});
+
+	var nBlocs = 3+Math.floor(Math.random()*3);
+	for(var i=0; i<nBlocs; i++){
+
+		var m = new THREE.Mesh(geom, mat);
+
+		m.position.x = i*15;
+		m.position.y = Math.random()*10;
+		m.position.z = Math.random()*10;
+		m.rotation.z = Math.random()*Math.PI*2;
+		m.rotation.y = Math.random()*Math.PI*2;
+
+		var s = .1 + Math.random()*.9;
+		m.scale.set(s,s,s);
+
+		m.castShadow = true;
+		m.receiveShadow = true;
+
+		this.mesh.add(m);
+	}
+}
+
+Sky = function(){
+	this.mesh = new THREE.Object3D();
+
+	this.nClouds = 20;
+
+	var stepAngle = Math.PI*2 / this.nClouds;
+
+	for(var i = 0; i < this.nClouds; i++){
+		var c = new Cloud();
+
+		var a = stepAngle*i;
+		var h = 750 + Math.random()*200;
+
+		c.mesh.position.y = Math.sin(a)*h;
+		c.mesh.position.x = Math.cos(a)*h;
+
+		c.mesh.rotation.z = a+Math.PI/2;
+
+		c.mesh.position.z = -400-Math.random()*400;
+
+		var s = 1+Math.random()*2;
+		c.mesh.scale.set(s, s, s);
+
+		this.mesh.add(c.mesh);
+	}
+}
+
+var sky;
+function createSky(){
+	sky = new Sky();
+	sky.mesh.position.y = -600;
+	scene.add(sky.mesh);
+}
+
+var AirPlane = function(){
+	this.mesh = new THREE.Object3D();
+
+	var geomCockpit = new THREE.BoxGeometry(60, 50, 50, 1, 1, 1);
+	var matCockpit = new THREE.MeshPhongMaterial({color:Colors.red, shading:THREE.FlatShading});
+	var cockpit = new THREE.Mesh(geomCockpit, matCockpit);
+	cockpit.castShadow = true;
+	cockpit.receiveShadow = true;
+	this.mesh.add(cockpit);
+
+	var geomEngine = new THREE.BoxGeometry(20,50,50,1,1,1);
+	var matEngine = new THREE.MeshPhongMaterial({color:Colors.white, shading:THREE.FlatShading});
+	var engine = new THREE.Mesh(geomEngine, matEngine);
+	engine.position.x = 40;
+	engine.castShadow = true;
+	engine.receiveShadow = true;
+	this.mesh.add(engine);
+
+	var geomTailPlane = new THREE.BoxGeometry(15, 20, 5, 1, 1, 1);
+	var matTailPlane = new THREE.MeshPhongMaterial({color:Colors.red, shading:THREE.FlatShading});
+	var tailPlane = new THREE.Mesh(geomTailPlane, matTailPlane);
+	tailPlane.position.set(-35, 25, 0);
+	tailPlane.castShadow = true;
+	tailPlane.receiveShadow = true;
+	this.mesh.add(tailPlane);
+
 }
